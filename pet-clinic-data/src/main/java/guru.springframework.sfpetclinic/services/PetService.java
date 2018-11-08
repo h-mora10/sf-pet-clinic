cf6722c52164c334acbe85b71b1ceb28d402c45d
Henry Mora <h.mora10@uniandes.edu.co>
2018-11-07 22:20:58 -0500

    Interfaces for services were created. Closes issue #5

---
+++ pet-clinic-data/src/main/java/guru.springframework.sfpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfpetclinic.services;
+
+import guru.springframework.sfpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet owner);
+
+    Set<Pet> findAll();
+}
